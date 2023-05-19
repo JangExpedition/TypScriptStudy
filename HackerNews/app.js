9fedc755b34052206769ba0fdcb8af7566fafb9b
Jangcccci <jangcccci@gmail.com>
2023-05-19 09:45:00 +0900

    라우터 구현

--- HackerNews/app.js
+++ HackerNews/app.js
@@ -1,5 +1,5 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
-const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
@@ -10,33 +10,51 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+    const newsFeed = getData(NEWS_URL);
+    const newsList = [];
 
-window.addEventListener('hashchange', function() {
-    const id = location.hash.substring(1);
+    newsList.push(`<ul>`);
 
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = document.createElement('h1');
+    for(let i = 0; i < 10; i ++){
+        newsList.push(`
+        <li>
+            <a href="#${newsFeed[i].id}">
+            ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+        `);
+    }
 
-    title.innerHTML = newsContent.title;
+    newsList.push(`</ul>`);
 
-    content.appendChild(title);
-});
-
-for(let i = 0; i < 10; i ++){
-    const div = document.createElement('div');
-
-    div.innerHTML = `
-    <li>
-        <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-    </li>
-    `;
-
-    ul.appendChild(div.firstElementChild);
+    container.innerHTML = newsList.join('');
 }
 
-document.getElementById('root').appendChild(ul);
-document.getElementById('root').appendChild(content);
+function newsDetail() {
+    const id = location.hash.substring(1);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+};
+
+function router() {
+    const routePath = location.hash;
+    console.log(routePath);
+
+    if(routePath === '') {
+        newsFeed();
+    }else{
+        newsDetail();
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
